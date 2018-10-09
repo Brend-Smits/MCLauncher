f8c9277a89903fd6e8300a32c909349351685468
Brend Smits <brend_smits@hotmail.com>
2018-10-09 10:57:33 +0200

    Implement basic CRUD functionality for API

--- MCLauncher.API/src/main/Java/net/toastynetworks/MCLauncher/API/Controller/ModpackController.java
+++ MCLauncher.API/src/main/Java/net/toastynetworks/MCLauncher/API/Controller/ModpackController.java
@@ -3,10 +3,8 @@ package net.toastynetworks.MCLauncher.API.Controller;
 import net.toastynetworks.MCLauncher.API.Models.Modpack;
 import net.toastynetworks.MCLauncher.API.Service.ModpackService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,5 +16,30 @@ public class ModpackController {
     @RequestMapping("/modpack")
     public List<Modpack> getAllModpacks() {
         return modpackService.getAllModpacks();
+    }
+
+    @RequestMapping("/modpack/{name}")
+    public Modpack getModpack(@PathVariable String name) {
+        return modpackService.getModpack(name);
+    }
+
+    @RequestMapping("/modpack/release/{releaseType}")
+    public List<Modpack> getModpacksWithReleaseType(@PathVariable String releaseType) {
+        return modpackService.getModpacksWithReleaseType(releaseType);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/modpack")
+    public void addModpack(@RequestBody Modpack modpack) {
+        modpackService.addModpack(modpack);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/modpack/{name}")
+    public void updateModpack(@RequestBody Modpack modpack, @PathVariable String name) {
+        modpackService.updateModpack(modpack, name);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "modpack/{name}")
+    public void deleteModpack(@PathVariable String name) {
+        modpackService.deleteModpack(name);
     }
 }
--- MCLauncher.API/src/main/Java/net/toastynetworks/MCLauncher/API/Service/ModpackService.java
+++ MCLauncher.API/src/main/Java/net/toastynetworks/MCLauncher/API/Service/ModpackService.java
@@ -2,20 +2,44 @@ package net.toastynetworks.MCLauncher.API.Service;
 
 import net.toastynetworks.MCLauncher.API.Models.Modpack;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ModpackService {
 
-    List<Modpack> modpacks = Arrays.asList(
+    List<Modpack> modpacks = new ArrayList<>(Arrays.asList(
             new Modpack("SF3", "Latest"),
+            new Modpack("TL", "Latest"),
             new Modpack("IE", "Beta")
+    )
     );
 
     public List<Modpack> getAllModpacks() {
         return modpacks;
     }
 
+    public Modpack getModpack(String name) {
+        return modpacks.stream().filter(t -> t.getName().equals(name)).findFirst().get();
+    }
+
+    public List<Modpack> getModpacksWithReleaseType(String releaseType) {
+            return modpacks.stream().filter(t -> t.getVersionType().equals(releaseType)).collect(Collectors.toList());
+    }
+
+    public void addModpack(Modpack modpack) {
+        modpacks.add(modpack);
+    }
+
+    public void updateModpack(Modpack modpack, String modpackName) {
+        modpacks.stream().filter(t -> t.getName().equals(modpackName)).findFirst().ifPresent(i -> {i.setName(modpack.getName());i.setVersionType(modpack.getVersionType());});
+    }
+
+    public void deleteModpack(String modpackName) {
+        modpacks.removeIf(t -> t.getName().equals(modpackName));
+    }
 }
