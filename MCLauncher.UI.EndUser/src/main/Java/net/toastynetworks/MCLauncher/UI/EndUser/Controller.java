74cfa665e55c7d98c2b36a01b5ecef6c40c996f8
Brend Smits <brend_smits@hotmail.com>
2018-10-16 16:23:11 +0200

    Connect User UI & Controller, fix git issues.
    There were some very weird issues where any file starting with user was not indexed by git. This has now been worked around, also fixed an issue where controller was not found in scene builder due to rescources folder. Unfortunately the event-autocompletion is still not working. It seems to be a limitation to Scene Builder. 
    Source: https://stackoverflow.com/questions/40332451/scene-builder-not-reconazing-my-controller-class

---
+++ MCLauncher.UI.EndUser/src/main/Java/net/toastynetworks/MCLauncher/UI/EndUser/Controller.java
@@ -0,0 +1,8 @@
+package net.toastynetworks.MCLauncher.UI.EndUser;
+
+public class Controller {
+
+    public void playButtonClicked(){
+        System.out.println("Used started modpack....");
+    }
+}
